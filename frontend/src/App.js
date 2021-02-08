ee17f534f992743c9f8cd8b500f0d1cf8a0eb406
Francesco Done <francesco.done.11597@gmail.com>
2021-02-08 17:02:59 +0100

    Added dynamic navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AdminLTE, { Sidebar } from 'adminlte-2-react';
+import Dashboard from "./Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Item } = Sidebar;
+
+class App extends Component {
+
+  sidebar = [
+    <Item key="1" text="Macchina di Massimiliano" to="/company1user1" />,
+    <Item key="2" text="Macchina di Francesco" to="/company1user2" />
+  ]
+
+  render() {
+    return (
+      <AdminLTE title={["ITCube", "Consulting"]} titleShort={["ITC", "C"]} theme="blue" sidebar={this.sidebar}>
+        {this.sidebar.map((item) => <Dashboard path={item.props.to} title={item.props.text} />)}
+      </AdminLTE>
+    );
+  }
 }
 
 export default App;
