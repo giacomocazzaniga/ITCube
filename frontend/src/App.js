865a7b6de490ab343a4e2a41551b280e1fd9b330
Francesco Done <francesco.done.11597@gmail.com>
2021-02-08 17:48:06 +0100

    Update dynamicity on sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,30 @@ import Dashboard from "./Dashboard";
 const { Item } = Sidebar;
 
 class App extends Component {
+  state = {
+    company_id : "ITCube Consulting",
+    client_list : [
+      {
+        id : "1",
+        name : "Macchina di Massimiliano"
+      },
+      {
+        id : "2",
+        name : "Macchina di Francesco"
+      }
+    ]
+  }
 
-  sidebar = [
-    <Item key="1" text="Macchina di Massimiliano" to="/company1user1" />,
-    <Item key="2" text="Macchina di Francesco" to="/company1user2" />
-  ]
+  getSidebar(client_list, company_id){
+    let item_list = [];
+    client_list.map((item) => (item_list = [item_list, <Item key={item.id} text={item.name} to={"/company"+company_id+"user"+item.id} />]));
+    return item_list;
+  }
 
   render() {
     return (
-      <AdminLTE title={["ITCube", "Consulting"]} titleShort={["ITC", "C"]} theme="blue" sidebar={this.sidebar}>
-        {this.sidebar.map((item) => <Dashboard path={item.props.to} title={item.props.text} />)}
+      <AdminLTE title={["ITCube", "Consulting"]} titleShort={["ITC", "C"]} theme="blue" sidebar={this.getSidebar(this.state.client_list, this.state.company_id)}>
+        {this.state.client_list.map((item) => <Dashboard path={"/company"+this.state.company_id+"user"+item.id} title={item.name} />)}
       </AdminLTE>
     );
   }
