d37b2abfca1393b05c5f73a1ebf2bb63c3db9303
Stefano Puglia <stich.puglia@gmail.com>
2021-03-25 09:11:39 +0100

    Export DB

--- backend/exportSQL.sql
+++ backend/exportSQL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 24, 2021 at 05:06 PM
+-- Generation Time: Mar 25, 2021 at 08:08 AM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
